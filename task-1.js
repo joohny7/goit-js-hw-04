f978ac95f6f3388c14a1f35d1103d467a6b01bf9
joohny7 <150726859+joohny7@users.noreply.github.com>
2024-08-25 16:51:03 +0200

    commit

---
+++ task-1.js
@@ -0,0 +1,14 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+
+  for (const quantity of Object.values(products)) {
+    totalQuantity += quantity;
+  }
+
+  return totalQuantity <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 4 }, 6)); // true
+console.log(isEnoughCapacity({ apples: 2, grapes: 4 }, 5)); // false
+console.log(isEnoughCapacity({ apples: 1, bananas: 2, oranges: 3 }, 10)); // true
+console.log(isEnoughCapacity({ apples: 5, bananas: 7 }, 10)); // false
