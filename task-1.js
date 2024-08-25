0ed19c18a33f3951082d6b2c7534ed8eb67e9259
joohny7 <150726859+joohny7@users.noreply.github.com>
2024-08-25 16:54:08 +0200

    commit

--- task-1.js
+++ task-1.js
@@ -12,3 +12,5 @@ console.log(isEnoughCapacity({ apples: 2, grapes: 4 }, 6)); // true
 console.log(isEnoughCapacity({ apples: 2, grapes: 4 }, 5)); // false
 console.log(isEnoughCapacity({ apples: 1, bananas: 2, oranges: 3 }, 10)); // true
 console.log(isEnoughCapacity({ apples: 5, bananas: 7 }, 10)); // false
+
+
